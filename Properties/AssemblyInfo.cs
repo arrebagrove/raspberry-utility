fed7d806350294ed9ee18184ec21b43d60a1b1a7
Aurelien Jacquot <aurelien.jacquot@hotmail.fr>
2016-08-29 05:30:12 +0200

    first commit

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,29 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("PhoebeCoeus.IoT.RaspberryUtility")]
+[assembly: AssemblyDescription("Utility tools for Win 10 IoT and Raspberry. Visit http://phoebecoeus.net/dev for more information.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Phoebe Coeus")]
+[assembly: AssemblyProduct("PhoebeCoeus.IoT.RaspberryUtility")]
+[assembly: AssemblyCopyright("Phoebe Coeus - Copyright ©  2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: ComVisible(false)]
